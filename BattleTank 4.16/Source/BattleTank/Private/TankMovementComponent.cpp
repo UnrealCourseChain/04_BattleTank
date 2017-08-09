681905359fbcc97a0c7ee393235fdca20de2cc72
David Hendry <chain_default@hotmail.com>
2017-08-08 23:25:25 -0600

    158 A Better Component Architecture

--- BattleTank 4.16/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank 4.16/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
 /// Intended movement behavior: Movement is relative to cursor location. Forward = towards cursor.
@@ -15,6 +16,10 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO Prevent double speed from double input
 }
 
